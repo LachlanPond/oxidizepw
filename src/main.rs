1a1a59dbbabd6936365f6e221cfd9ddc85fe61b9
Lachlan Pond <lachipond@gmail.com>
2024-02-18 11:59:05 +1030

    Set up database/password structures and basic cmd-line parsing
    Databases have been setup to be loaded and saved to a file. The
    command line parsing only serves to check basic existance of valid
    option, but proper value checking still needs to be implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::process;
+
+use oxidizepw::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = oxidizepw::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
