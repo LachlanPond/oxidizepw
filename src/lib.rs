7a37b6dc9f86ac4bae629fa763af758e7d885c14
Lachlan Pond <lachipond@gmail.com>
2024-02-23 20:02:41 +1030

    Unbuffer new database master password input

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ mod database;
 pub mod config;
 
 use std::error::Error;
-use std::io;
 
 use crate::config::Config;
 use crate::config::Command;
@@ -18,11 +17,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     match config.command {
         config::Command::None => {
-            println!("Please enter the master password for the new database");
-            let mut master_password = String::new();
-            io::stdin()
-                .read_line(&mut master_password)
-                .expect("Failed to read line");
+            let master_password = rpassword::prompt_password("Please enter the master password for the new database\n").unwrap();
             Database::new(config.database_name, master_password)?;
             return Ok(())
         },
